419a25b6adc32c62db22060e0c9168d882de48b2
gyro永不抽风 <1247006353@qq.com>
2021-12-15 01:20:28 +0800

    feat: support series generation

--- cli.py
+++ cli.py
@@ -6,6 +6,7 @@ import traceback
 import os
 import requests
 import opencc
+import js2py
 from bs4 import BeautifulSoup
 
 from utils import downloader
@@ -18,9 +19,9 @@ DUMP_PATH = './dump'
 echo.init_subroutine()
 
 
-def download_content(url):
+def download_webpage(url):
 	'''
-	download web content
+	Download webpage from url.
 	:param url: url to download
 	'''
 	echo.push_subroutine(sys._getframe().f_code.co_name)
@@ -28,7 +29,7 @@ def download_content(url):
 	echo.clog(f'start downloading: {url} => memory')
 	# download
 	try:
-		res = requests.get(url=url, headers={
+		return requests.get(url=url, headers={
 			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 			'accept-encoding': 'gzip, deflate, br',
 			'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
@@ -47,6 +48,43 @@ def download_content(url):
 		echo.cerr(f'error: {repr(e)}')
 		traceback.print_exc()
 		echo.cexit('DOWNLOAD FAILED')
+
+
+def process_series_page(url):
+	'''
+	Process the series page.
+	:param url: url to process
+	'''
+	echo.push_subroutine(sys._getframe().f_code.co_name)
+
+	res = download_webpage(url)
+	# parse
+	try:
+		soup = BeautifulSoup(res, 'lxml')
+		scripts = soup.find_all('script')
+
+		for script in scripts:
+			if script.string is not None:
+				if script.string.startswith('window.__NUXT__'):
+					context = js2py.EvalJs()
+					context.execute(script.string)
+					
+					data = context.window.__NUXT__.to_dict()
+					return data['data'][0]['series']['articles']
+	except Exception as e:
+		echo.cerr(f'error: {repr(e)}')
+		traceback.print_exc()
+		echo.cexit('PARSING FAILED')
+
+
+def obtain_article_content(url):
+	'''
+	download web content
+	:param url: url to download
+	'''
+	echo.push_subroutine(sys._getframe().f_code.co_name)
+
+	res = download_webpage(url)
 	# parse
 	try:
 		soup = BeautifulSoup(res, 'lxml')
@@ -97,10 +135,48 @@ def download_images(content, dump_path):
 				# update html
 				with open(dump_path, 'w', encoding='utf-8') as f:
 					f.write(str(soup))
+		return str(soup)
 	except Exception as e:
 		echo.cerr(f'error: {repr(e)}')
 		traceback.print_exc()
 		echo.cexit('DOWNLOAD FAILED')
+
+
+def process_article_page(url, dump_path, cvt=None):
+	'''
+	Process the article page.
+	:param url: url to process
+	:param dump_path: path to dump html
+	:return: string of html content
+	'''
+	echo.push_subroutine(sys._getframe().f_code.co_name)
+
+	try:
+		# obtain html
+		if dump_path is not None:
+			if os.path.exists(dump_path):
+				with open(dump_path, 'r', encoding='utf-8') as f:
+					content = f.read()
+		else:
+			content = obtain_article_content(url)
+			if dump_path is None:
+				dump_path = os.path.join(DUMP_PATH, f'./{time.time_ns()}.html')
+			with open(dump_path, 'w', encoding='utf-8') as f:
+				f.write(content)
+			echo.clog(f'Dumped HTML to {dump_path}')
+		# download images
+		content = download_images(content, dump_path)
+
+		# chinese conversion
+		if cvt in ["s2t", "t2s", "s2tw", "tw2s", "s2hk", "hk2s", "s2twp", "tw2sp", "t2tw", "hk2t", "t2hk", "t2jp", "jp2t", "tw2t"]:
+			converter = opencc.OpenCC(f'{cvt}.json')
+			content = converter.convert(content)
+
+		return content
+	except Exception as e:
+		echo.cerr(f'error: {repr(e)}')
+		traceback.print_exc()
+		echo.cexit('PROCESS PAGE FAILED')
 
 
 @click.group()
@@ -146,20 +222,31 @@ def download(dump_path,
 
 	try:
 		# init directory
-		if not os.path.exists(dump_path):
-			os.mkdir(dump_path)
-		# obtain html
-		if html_dump is not None:
-			with open(html_dump, 'r', encoding='utf-8') as f:
-				content = f.read()
+		if not os.path.exists(DUMP_PATH):
+			os.mkdir(DUMP_PATH)
+
+		if 'series' in url:
+			contents = []
+			# series
+			articles = process_series_page(url)
+			i = 0
+			for article in articles:
+				i += 1
+				echo.clog(f'Processing article {i} / {len(articles)}')
+				a_title = article["title"]
+				aid = article["aid"]
+				echo.clog(f'Title: {a_title}')
+				echo.clog(f'Article ID: {aid}')
+				a_url = f'https://www.lightnovel.us/cn/detail/{aid}/'
+				a_dump_path = os.path.join(DUMP_PATH, f'{aid}.html')
+				a_content = process_article_page(a_url, a_dump_path, cvt)
+				contents.append({
+					'title': a_title,
+					'content': a_content
+				})
 		else:
-			content = download_content(url)
-			html_dump = os.path.join(DUMP_PATH, f'./{time.time_ns()}.html')
-			with open(html_dump, 'w', encoding='utf-8') as f:
-				f.write(content)
-			echo.clog(f'Dumped HTML to {html_dump}')
-		# download images
-		download_images(content, html_dump)
+			# article
+			contents = process_article_page(url, html_dump)
 
 		if title is None:
 			title = input('Input title of light novel: ')
@@ -170,12 +257,7 @@ def download(dump_path,
 		if cover_link is None:
 			cover_link = input('(optional) Input cover_link of light novel (see --help for further explanation): ')
 		novel = LightNovel(url=url, authors=authors.split(','), identifier=identifier, title=title, cover_link=cover_link)
-		novel.content = content
-
-		if cvt in ["s2t", "t2s", "s2tw", "tw2s", "s2hk", "hk2s", "s2twp", "tw2sp", "t2tw", "hk2t", "t2hk", "t2jp", "jp2t", "tw2t"]:
-			converter = opencc.OpenCC(f'{cvt}.json')
-			novel.content = converter.convert(novel.content)
-		
+		novel.contents = contents
 		novel.write_epub(path)
 	except Exception as e:
 		echo.cerr(f'Error: {repr(e)}')
