1582bc75f2a769e35ab7509239bc6212db1cbc95
gyro永不抽风 <1247006353@qq.com>
2021-12-14 19:07:26 +0800

    refactor: move download_content to cli.py

--- cli.py
+++ cli.py
@@ -1,10 +1,58 @@
 from lightnovel import LightNovel
 import click
+import time
+import sys
+import traceback
+import os
+import requests
 import opencc
+from bs4 import BeautifulSoup
 
 from utils import echo
 
 echo.init_subroutine()
+
+
+def download_content(url):
+	'''
+	download web content
+	'''
+	echo.push_subroutine(sys._getframe().f_code.co_name)
+
+	echo.clog(f'start downloading: {url} => memory')
+	# download
+	try:
+		res = requests.get(url=url, headers={
+			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+			'accept-encoding': 'gzip, deflate, br',
+			'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+			'cache-control': 'no-cache',
+			'pragma': 'no-cache',
+			'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Microsoft Edge";v="92"',
+			'sec-ch-ua-mobile': '?0',
+			'sec-fetch-dest': 'document',
+			'sec-fetch-mode': 'navigate',
+			'sec-fetch-site': 'same-origin',
+			'sec-fetch-user': '?1',
+			'upgrade-insecure-requests': '1',
+			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84'
+		}).text
+	except Exception as e:
+		echo.cerr(f'error: {repr(e)}')
+		traceback.print_exc()
+		echo.cexit('DOWNLOAD FAILED')
+	# parse
+	try:
+		soup = BeautifulSoup(res, 'lxml')
+		article = soup.find('article', id='article-main-contents')
+	except Exception as e:
+		echo.cerr(f'error: {repr(e)}')
+		traceback.print_exc()
+		echo.cexit('PARSING FAILED')
+
+	return str(article)
+	echo.pop_subroutine()
+
 
 @click.group()
 def cli():
@@ -46,6 +94,19 @@ def download(dump_path,
 	echo.push_subroutine(sys._getframe().f_code.co_name)
 
 	try:
+		# init directory
+		if not os.path.exists(dump_path):
+			os.mkdir(dump_path)
+		if html_dump is not None:
+			with open(html_dump, 'r', encoding='utf-8') as f:
+				content = f.read()
+		else:
+			content = download_content(url)
+			html_path = os.path.join(dump_path, f'./{time.time_ns()}.html')
+			with open(html_path, 'w', encoding='utf-8') as f:
+				f.write(content)
+			echo.clog(f'Dumped HTML to {html_path}')
+		
 		if title is None:
 			title = input('Input title of light novel: ')
 		if authors is None:
@@ -55,10 +116,12 @@ def download(dump_path,
 		if cover_link is None:
 			cover_link = input('(optional) Input cover_link of light novel (see --help for further explanation): ')
 		novel = LightNovel(url=url, authors=authors.split(','), identifier=identifier, title=title, cover_link=cover_link)
-		novel.download_content()
+		novel.content = content
+
 		if cvt in ["s2t", "t2s", "s2tw", "tw2s", "s2hk", "hk2s", "s2twp", "tw2sp", "t2tw", "hk2t", "t2hk", "t2jp", "jp2t", "tw2t"]:
 			converter = opencc.OpenCC(f'{cvt}.json')
 			novel.content = converter.convert(novel.content)
+		
 		novel.write_epub(path)
 	except Exception as e:
 		echo.cerr(f'Error: {repr(e)}')
--- lightnovel.py
+++ lightnovel.py
@@ -58,47 +58,6 @@ class LightNovel():
 		if cover_link is not None: self.cover_link = cover_link
 
 
-	def download_content(self):
-		'''
-		download web content
-		'''
-		echo.push_subroutine(sys._getframe().f_code.co_name)
-
-		echo.clog(f'start downloading: {self.url} => memory')
-		# download
-		try:
-			res = requests.get(url=self.url, headers={
-				'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-				'accept-encoding': 'gzip, deflate, br',
-				'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-				'cache-control': 'no-cache',
-				'pragma': 'no-cache',
-				'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Microsoft Edge";v="92"',
-				'sec-ch-ua-mobile': '?0',
-				'sec-fetch-dest': 'document',
-				'sec-fetch-mode': 'navigate',
-				'sec-fetch-site': 'same-origin',
-				'sec-fetch-user': '?1',
-				'upgrade-insecure-requests': '1',
-				'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84'
-			}).text
-		except Exception as e:
-			echo.cerr(f'error: {repr(e)}')
-			traceback.print_exc()
-			echo.cexit('DOWNLOAD FAILED')
-		# parse
-		try:
-			soup = BeautifulSoup(res, 'lxml')
-			article = soup.find('article', id='article-main-contents')
-			self.content = str(article)
-		except Exception as e:
-			echo.cerr(f'error: {repr(e)}')
-			traceback.print_exc()
-			echo.cexit('PARSING FAILED')
-
-		echo.pop_subroutine()
-
-
 	def write_epub(self, path: str):
 		'''
 		generate the ebook to `path`
